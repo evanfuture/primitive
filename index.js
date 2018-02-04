a0acaf1de7d93dba39487b4ce2beb7b89bcfdaf8
Evan Payne <evan.payne@netcentric.biz>
2018-02-04 21:54:01 +0100

    Adds ability to run on linux as well as mac.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ module.exports = function(fileName, shapeCount = 100, shapeType = 1) {
     return new Promise((resolve, reject) => {
         let svg = '';
         const svgPath = fileName.replace(/\.[^/.]+$/, '.svg');
-        const command = __dirname + '/./main';
+        const binary = process.platform === 'darwin' ? 'main' : 'main-linux';
+        const command = __dirname + '/./' + binary;
         const child = spawn(command, ['-i', fileName, '-o', svgPath, '-p', '-n', shapeCount, '-m', shapeType]);
 
         child.stdout.on('data', function(data) {
